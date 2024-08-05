4674c0f4c83aa7d6878fac4dcd7d3954868132ee
rishi <rishijash@hotmail.com>
2024-08-05 13:45:27 -0700

    Support Icon on VS Code

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,32 @@
 import * as vscode from 'vscode';
 import { runTest } from './commands/runTest';
 
+class LLMGateProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
+  getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
+    return element;
+  }
+
+  getChildren(element?: vscode.TreeItem): Thenable<vscode.TreeItem[]> {
+    if (element) {
+      return Promise.resolve([]);
+    } else {
+      const runTestItem = new vscode.TreeItem('Run Prompt Lab 🧪');
+      runTestItem.command = { command: 'llmgate.runTest', title: 'Run Prompt Lab' };
+      return Promise.resolve([runTestItem]);
+    }
+  }
+}
+
 export function activate(context: vscode.ExtensionContext) {
     console.log('Wohoo, your extension "LLMGate" is now active!');
 
     let disposable = vscode.commands.registerCommand('llmgate.runTest', runTest);
 
     context.subscriptions.push(disposable);
+
+    // Register the tree data provider
+    const llmGateProvider = new LLMGateProvider();
+    vscode.window.registerTreeDataProvider('llmgate-view', llmGateProvider);
 }
 
 export function deactivate() {}
