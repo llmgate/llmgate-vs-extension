19d700387dea5ff8e08ff1d492ecaa11144d5018
rishi <rishijash@hotmail.com>
2024-08-05 13:51:40 -0700

    Settings in the Tree

--- src/extension.ts
+++ src/extension.ts
@@ -12,7 +12,11 @@ class LLMGateProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
     } else {
       const runTestItem = new vscode.TreeItem('Run Prompt Lab 🧪');
       runTestItem.command = { command: 'llmgate.runTest', title: 'Run Prompt Lab' };
-      return Promise.resolve([runTestItem]);
+
+      const settingsItem = new vscode.TreeItem('Settings');
+      settingsItem.command = { command: 'llmgate.openSettings', title: 'Set LLM Keys' };
+
+      return Promise.resolve([runTestItem, settingsItem]);
     }
   }
 }
@@ -23,6 +27,12 @@ export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('llmgate.runTest', runTest);
 
     context.subscriptions.push(disposable);
+
+
+    let openSettingsDisposable = vscode.commands.registerCommand('llmgate.openSettings', () => {
+      vscode.commands.executeCommand('workbench.action.openSettings', 'llmgate');
+    });
+    context.subscriptions.push(openSettingsDisposable);
 
     // Register the tree data provider
     const llmGateProvider = new LLMGateProvider();
